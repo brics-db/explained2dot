88be62a6b055616be576db95eb9477580c88604d
Till Kolditz <Till.Kolditz@gmail.com>
2017-06-01 16:24:29 +0200

    add files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,43 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=explained2dot
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/explained2dot
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=explained2dot.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/explained2dot.tar
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=explained2dot
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/explained2dot
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=explained2dot.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/explained2dot.tar
+# Verbose configuration
+CND_PLATFORM_Verbose=GNU-Linux
+CND_ARTIFACT_DIR_Verbose=dist/Verbose/GNU-Linux
+CND_ARTIFACT_NAME_Verbose=explained2dot
+CND_ARTIFACT_PATH_Verbose=dist/Verbose/GNU-Linux/explained2dot
+CND_PACKAGE_DIR_Verbose=dist/Verbose/GNU-Linux/package
+CND_PACKAGE_NAME_Verbose=explained2dot.tar
+CND_PACKAGE_PATH_Verbose=dist/Verbose/GNU-Linux/package/explained2dot.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
